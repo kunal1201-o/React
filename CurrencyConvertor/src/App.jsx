103883cfe12bf50770a3471890000dc50fdac5fc
kunal1201-o <12012000kunalsingh@gmail.com>
2023-11-13 12:23:35 +0530

    resolve bug

--- CurrencyConvertor/src/App.jsx
+++ CurrencyConvertor/src/App.jsx
@@ -29,7 +29,7 @@ return (
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
           backgroundImage: `url('https://images.pexels.com/photos/4025825/pexels-photo-4025825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
-      }}https
+      }}
   >
       <div className="w-full">
           <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
